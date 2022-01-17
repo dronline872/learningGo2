6f6ad447aae2eaf3700b13c7e1fb999f9b7cbd3d
Alexander Sharov <dronline872@gmail.com>
2022-01-17 23:11:54 +0300

    Урок 3

---
+++ go.mod
@@ -0,0 +1,5 @@
+module golang2
+
+go 1.17
+
+require github.com/dronline872/lessonpackage/v2 v2.0.0
---
+++ lesson_3/lesson_3.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	lessonpackage "github.com/dronline872/lessonpackage/v2/lesson_package"
+)
+
+func main() {
+	lessonpackage.Test()
+	fmt.Println("123")
+}
